cf46f3a762030f8cff79e5cf4f44acac2e5a733d
Chien <cvoquang@gmail.com>
2022-05-08 14:42:34 +0700

    Fullstack ReactJs Video 53 (#44) Design Display & Header

--- src/containers/System/ModalEditUser.js
+++ src/containers/System/ModalEditUser.js
@@ -3,228 +3,197 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import _ from "lodash";
 import {
-    Modal,
-    ModalBody,
-    ModalFooter,
-    ModalHeader,
-    Button,
-    Form,
-    Row,
-    Col,
-    FormGroup,
-    Label,
-    Input,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Button,
+  Form,
+  Row,
+  Col,
+  FormGroup,
+  Label,
+  Input,
 } from "reactstrap";
 
 class ModalEditUser extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: "",
-            email: "",
-            password: "",
-            firstName: "",
-            lastName: "",
-            address: "",
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      address: "",
+    };
+  }
+
+  componentDidMount() {
+    //   let {currentUser} = this.props
+    let user = this.props.currentUser;
+    if (user && !_.isEmpty(user)) {
+      this.setState({
+        id: user.id,
+        email: user.email,
+        password: "123456",
+        firstName: user.firstName,
+        lastName: user.lastName,
+        address: user.address,
+      });
     }
+  }
 
-    componentDidMount() {
-        //   let {currentUser} = this.props
-        let user = this.props.currentUser;
-        if (user && !_.isEmpty(user)) {
-            this.setState({
-                id: user.id,
-                email: user.email,
-                password: "123456",
-                firstName: user.firstName,
-                lastName: user.lastName,
-                address: user.address,
-            });
-        }
+  handletoggle = () => {
+    this.props.toggleFormParent();
+  };
+
+  handleOnChangeInput = (event, id) => {
+    let copyState = { ...this.state };
+    copyState[id] = event.target.value;
+    this.setState({
+      ...copyState,
+    });
+  };
+
+  checkValidateInput = () => {
+    let isValid = true;
+    let arrInput = ["email", "password", "firstName", "lastName", "address"];
+    for (let i = 0; i < arrInput.length; i++) {
+      if (!this.state[arrInput[i]]) {
+        isValid = false;
+        alert("Missing parametter: " + arrInput[i]);
+        break;
+      }
     }
+    return isValid;
+  };
 
-    handletoggle = () => {
-        this.props.toggleFormParent();
-    };
+  handleSaveUser = () => {
+    let isValid = this.checkValidateInput();
+    if (isValid === true) {
+      this.props.editUser(this.state);
+      //   this.setState({
+      //     email: "",
+      //     password: "",
+      //     firstName: "",
+      //     lastName: "",
+      //     address: "",
+      //   });
+    }
+  };
 
-    handleOnChangeInput = (event, id) => {
-        let copyState = {...this.state };
-        copyState[id] = event.target.value;
-        this.setState({
-            ...copyState,
-        });
-    };
-
-    checkValidateInput = () => {
-        let isValid = true;
-        let arrInput = ["email", "password", "firstName", "lastName", "address"];
-        for (let i = 0; i < arrInput.length; i++) {
-            if (!this.state[arrInput[i]]) {
-                isValid = false;
-                alert("Missing parametter: " + arrInput[i]);
-                break;
-            }
-        }
-        return isValid;
-    };
-
-    handleSaveUser = () => {
-        let isValid = this.checkValidateInput();
-        if (isValid === true) {
-            this.props.editUser(this.state);
-            //   this.setState({
-            //     email: "",
-            //     password: "",
-            //     firstName: "",
-            //     lastName: "",
-            //     address: "",
-            //   });
-        }
-    };
-
-    render() {
-        return ( <
-            div className = "text-center" >
-            <
-            Modal isOpen = { this.props.isOpen }
-            toggle = {
-                () => {
-                    this.handletoggle();
-                }
-            } >
-            <
-            ModalHeader toggle = {
-                () => {
-                    this.handletoggle();
-                }
-            } >
-            Edit a user { " " } <
-            /ModalHeader>{" "} <
-            ModalBody >
-            <
-            Form >
-            <
-            Row >
-            <
-            Col md = { 6 } >
-            <
-            FormGroup >
-            <
-            Label
-            for = "exampleEmail" > Email < /Label>{" "} <
-            Input id = "exampleEmail"
-            value = { this.state.email }
-            onChange = {
-                (event) => {
-                    this.handleOnChangeInput(event, "email");
-                }
-            }
-            type = "text"
-            disabled /
-            >
-            <
-            /FormGroup>{" "} <
-            /Col>{" "} <
-            Col md = { 6 } >
-            <
-            FormGroup >
-            <
-            Label
-            for = "examplePassword" > Password < /Label>{" "} <
-            Input id = "examplePassword"
-            value = { this.state.password }
-            onChange = {
-                (event) => {
-                    this.handleOnChangeInput(event, "password");
-                }
-            }
-            type = "password"
-            disabled /
-            >
-            <
-            /FormGroup>{" "} <
-            /Col>{" "} <
-            /Row>{" "} <
-            Row >
-            <
-            Col md = { 6 } >
-            <
-            FormGroup >
-            <
-            Label
-            for = "examplefirstName" > FirstName < /Label>{" "} <
-            Input id = "examplefirstName"
-            value = { this.state.firstName }
-            onChange = {
-                (event) => {
-                    this.handleOnChangeInput(event, "firstName");
-                }
-            }
-            type = "text" /
-            >
-            <
-            /FormGroup>{" "} <
-            /Col>{" "} <
-            Col md = { 6 } >
-            <
-            FormGroup >
-            <
-            Label
-            for = "examplelastName" > LastName < /Label>{" "} <
-            Input id = "examplelastName"
-            value = { this.state.lastName }
-            onChange = {
-                (event) => {
-                    this.handleOnChangeInput(event, "lastName");
-                }
-            }
-            type = "text" /
-            >
-            <
-            /FormGroup>{" "} <
-            /Col>{" "} <
-            /Row>{" "} <
-            FormGroup >
-            <
-            Label
-            for = "exampleAddress" > Address < /Label>{" "} <
-            Input id = "exampleAddress"
-            value = { this.state.address }
-            onChange = {
-                (event) => {
+  render() {
+    return (
+      <div className="text-center">
+        <Modal
+          isOpen={this.props.isOpen}
+          toggle={() => {
+            this.handletoggle();
+          }}
+        >
+          <ModalHeader
+            toggle={() => {
+              this.handletoggle();
+            }}
+          >
+            Edit a user{" "}
+          </ModalHeader>{" "}
+          <ModalBody>
+            <Form>
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="exampleEmail"> Email </Label>{" "}
+                    <Input
+                      id="exampleEmail"
+                      value={this.state.email}
+                      onChange={(event) => {
+                        this.handleOnChangeInput(event, "email");
+                      }}
+                      type="text"
+                      disabled
+                    />
+                  </FormGroup>{" "}
+                </Col>{" "}
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="examplePassword"> Password </Label>{" "}
+                    <Input
+                      id="examplePassword"
+                      value={this.state.password}
+                      onChange={(event) => {
+                        this.handleOnChangeInput(event, "password");
+                      }}
+                      type="password"
+                      disabled
+                    />
+                  </FormGroup>{" "}
+                </Col>{" "}
+              </Row>{" "}
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="examplefirstName"> FirstName </Label>{" "}
+                    <Input
+                      id="examplefirstName"
+                      value={this.state.firstName}
+                      onChange={(event) => {
+                        this.handleOnChangeInput(event, "firstName");
+                      }}
+                      type="text"
+                    />
+                  </FormGroup>{" "}
+                </Col>{" "}
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="examplelastName"> LastName </Label>{" "}
+                    <Input
+                      id="examplelastName"
+                      value={this.state.lastName}
+                      onChange={(event) => {
+                        this.handleOnChangeInput(event, "lastName");
+                      }}
+                      type="text"
+                    />
+                  </FormGroup>{" "}
+                </Col>{" "}
+              </Row>{" "}
+              <FormGroup>
+                <Label for="exampleAddress"> Address </Label>{" "}
+                <Input
+                  id="exampleAddress"
+                  value={this.state.address}
+                  onChange={(event) => {
                     this.handleOnChangeInput(event, "address");
-                }
-            }
-            />{" "} <
-            /FormGroup>{" "} <
-            /Form>{" "} <
-            /ModalBody>{" "} <
-            ModalFooter >
-            <
-            Button color = "primary"
-            onClick = {
-                () => {
-                    this.handleSaveUser();
-                }
-            } >
-            Save changes { " " } <
-            /Button>{" "} <
-            Button onClick = {
-                function noRefCheck() {} } > Cancel < /Button>{" "} <
-            /ModalFooter>{" "} <
-            /Modal>{" "} <
-            /div>
-        );
-    }
+                  }}
+                />{" "}
+              </FormGroup>{" "}
+            </Form>{" "}
+          </ModalBody>{" "}
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => {
+                this.handleSaveUser();
+              }}
+            >
+              Save changes{" "}
+            </Button>{" "}
+            <Button onClick={function noRefCheck() {}}> Cancel </Button>{" "}
+          </ModalFooter>{" "}
+        </Modal>{" "}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {};
+  return {};
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModalEditUser);
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,205 +3,190 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
 import {
-    getAllUsers,
-    createNewUserService,
-    deleteUserService,
-    editUserService,
+  getAllUsers,
+  createNewUserService,
+  deleteUserService,
+  editUserService,
 } from "../../services/userService";
 import ModalUser from "./ModalUser";
 import ModalEditUser from "./ModalEditUser";
 class UserManage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            arrUsers: [],
-            isOpenModalUser: false,
-            isOpenModalEditUser: false,
-            userEdit: {},
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+      isOpenModalUser: false,
+      isOpenModalEditUser: false,
+      userEdit: {},
+    };
+  }
 
-    async componentDidMount() {
+  async componentDidMount() {
+    await this.getAllUsersFromReact();
+  }
+
+  getAllUsersFromReact = async () => {
+    let response = await (await getAllUsers("All")).data;
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
+    }
+  };
+
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser: true,
+    });
+  };
+
+  handleEditUser = (user) => {
+    // console.log("chien3: ", user);
+    this.setState({
+      isOpenModalEditUser: true,
+      userEdit: user,
+    });
+  };
+
+  doEditUser = async (user) => {
+    try {
+      let res = await (await editUserService(user)).data;
+      if (res && res.errCode === 0) {
         await this.getAllUsersFromReact();
+        this.setState({
+          isOpenModalEditUser: false,
+        });
+      } else {
+        alert(res.errCode);
+      }
+    } catch (error) {
+      console.log(error);
     }
+    // console.log("chien2: ", res);
+  };
 
-    getAllUsersFromReact = async() => {
-        let response = await (await getAllUsers("All")).data;
-        if (response && response.errCode === 0) {
-            this.setState({
-                arrUsers: response.users,
-            });
-        }
-    };
+  toggleModalUser = () => {
+    this.setState({
+      isOpenModalUser: !this.state.isOpenModalUser,
+    });
+  };
 
-    handleAddNewUser = () => {
+  toggleModalEditUser = () => {
+    this.setState({
+      isOpenModalEditUser: !this.state.isOpenModalEditUser,
+    });
+  };
+
+  createNewUser = async (data) => {
+    try {
+      let responseData = await createNewUserService(data);
+      let response = responseData.data;
+      if (response && response.errCode !== 0) {
+        alert(response.errMessage);
+      } else {
         this.setState({
-            isOpenModalUser: true,
+          isOpenModalUser: false,
         });
-    };
+        await this.getAllUsersFromReact();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    handleEditUser = (user) => {
-        // console.log("chien3: ", user);
-        this.setState({
-            isOpenModalEditUser: true,
-            userEdit: user,
-        });
-    };
+  handleDeleteUser = async (user) => {
+    try {
+      let response = await (await deleteUserService(user.id)).data;
+      if (response && response.errCode === 0) {
+        await this.getAllUsersFromReact();
+      } else {
+        alert(response.errMessage);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    doEditUser = async(user) => {
-        try {
-            let res = await (await editUserService(user)).data;
-            if (res && res.errCode === 0) {
-                await this.getAllUsersFromReact();
-                this.setState({
-                    isOpenModalEditUser: false,
-                });
-            } else {
-                alert(res.errCode);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-        // console.log("chien2: ", res);
-    };
+  render() {
+    let arrUsers = this.state.arrUsers;
 
-    toggleModalUser = () => {
-        this.setState({
-            isOpenModalUser: !this.state.isOpenModalUser,
-        });
-    };
-
-    toggleModalEditUser = () => {
-        this.setState({
-            isOpenModalEditUser: !this.state.isOpenModalEditUser,
-        });
-    };
-
-    createNewUser = async(data) => {
-        try {
-            let responseData = await createNewUserService(data);
-            let response = responseData.data;
-            if (response && response.errCode !== 0) {
-                alert(response.errMessage);
-            } else {
-                this.setState({
-                    isOpenModalUser: false,
-                });
-                await this.getAllUsersFromReact();
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    handleDeleteUser = async(user) => {
-        try {
-            let response = await (await deleteUserService(user.id)).data;
-            if (response && response.errCode === 0) {
-                await this.getAllUsersFromReact();
-            } else {
-                alert(response.errMessage);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    render() {
-        let arrUsers = this.state.arrUsers;
-
-        return ( <
-            >
-            <
-            div className = "" >
-            <
-            ModalUser isOpen = { this.state.isOpenModalUser }
-            toggleFormParent = { this.toggleModalUser }
-            createNewUser = { this.createNewUser }
-            />{" "} {
-                this.state.isOpenModalEditUser && ( <
-                    ModalEditUser isOpen = { this.state.isOpenModalEditUser }
-                    toggleFormParent = { this.toggleModalEditUser }
-                    currentUser = { this.state.userEdit }
-                    editUser = { this.doEditUser }
-                    />
-                )
-            } { " " } <
-            div className = "text-center" >
-            <
-            h1 > All Users < /h1>{" "} <
-            /div>{" "} <
-            div className = "mx-1" >
-            <
-            button className = "btn btn-primary px-3"
-            onClick = {
-                () => this.handleAddNewUser() } >
-            <
-            i className = "fas fa-plus" > < /i> Add new User{" "} <
-            /button>{" "} <
-            /div>{" "} <
-            div >
-            <
-            table id = "customers" >
-            <
-            tbody >
-            <
-            tr >
-            <
-            th > Email < /th> <th> First name </th > < th > Last name < /th>{" "} <
-            th > Address < /th> <th> Actions </th >
-            <
-            /tr>{" "} {
-                arrUsers &&
-                    arrUsers.length > 0 &&
-                    arrUsers.map((item, index) => {
-                        return ( <
-                            tr key = { index } >
-                            <
-                            td > { item.email } < /td> <td> {item.firstName} </td >
-                            <
-                            td > { item.lastName } < /td> <td> {item.address} </td >
-                            <
-                            td >
-                            <
-                            button className = "btn-edit"
-                            onClick = {
-                                () => {
-                                    this.handleEditUser(item);
-                                }
-                            } >
-                            <
-                            i className = "fas fa-pencil-alt" > < /i>{" "} <
-                            /button>{" "} <
-                            button className = "btn-delete"
-                            onClick = {
-                                () => {
-                                    this.handleDeleteUser(item);
-                                }
-                            } >
-                            <
-                            i className = "fas fa-trash" > < /i>{" "} <
-                            /button>{" "} <
-                            /td>{" "} <
-                            /tr>
-                        );
-                    })
-            } { " " } <
-            /tbody>{" "} <
-            /table>{" "} <
-            /div>{" "} <
-            /div>{" "} <
+    return (
+      <>
+        <div className="">
+          <ModalUser
+            isOpen={this.state.isOpenModalUser}
+            toggleFormParent={this.toggleModalUser}
+            createNewUser={this.createNewUser}
+          />{" "}
+          {this.state.isOpenModalEditUser && (
+            <ModalEditUser
+              isOpen={this.state.isOpenModalEditUser}
+              toggleFormParent={this.toggleModalEditUser}
+              currentUser={this.state.userEdit}
+              editUser={this.doEditUser}
             />
-        );
-    }
+          )}{" "}
+          <div className="text-center">
+            <h1> All Users </h1>{" "}
+          </div>{" "}
+          <div className="mx-1">
+            <button
+              className="btn btn-primary px-3"
+              onClick={() => this.handleAddNewUser()}
+            >
+              <i className="fas fa-plus"> </i> Add new User{" "}
+            </button>{" "}
+          </div>{" "}
+          <div>
+            <table id="customers">
+              <tbody>
+                <tr>
+                  <th> Email </th> <th> First name </th> <th> Last name </th>{" "}
+                  <th> Address </th> <th> Actions </th>
+                </tr>{" "}
+                {arrUsers &&
+                  arrUsers.length > 0 &&
+                  arrUsers.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td> {item.email} </td> <td> {item.firstName} </td>
+                        <td> {item.lastName} </td> <td> {item.address} </td>
+                        <td>
+                          <button
+                            className="btn-edit"
+                            onClick={() => {
+                              this.handleEditUser(item);
+                            }}
+                          >
+                            <i className="fas fa-pencil-alt"> </i>{" "}
+                          </button>{" "}
+                          <button
+                            className="btn-delete"
+                            onClick={() => {
+                              this.handleDeleteUser(item);
+                            }}
+                          >
+                            <i className="fas fa-trash"> </i>{" "}
+                          </button>{" "}
+                        </td>{" "}
+                      </tr>
+                    );
+                  })}{" "}
+              </tbody>{" "}
+            </table>{" "}
+          </div>{" "}
+        </div>{" "}
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {};
+  return {};
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
