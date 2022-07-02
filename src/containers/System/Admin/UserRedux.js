fb27f49cc0fbd024c008b52d4ea40b1ddd647a2e
Chien <cvoquang@gmail.com>
2022-07-02 08:49:02 +0700

    FSt ReactJs Video 71 (#62) new FileReader().readAsDataURL(file)

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
-import { LANGUAGES, CRUD_ACTIONS } from "../../../utils";
+import { LANGUAGES, CRUD_ACTIONS, CommonUtils } from "../../../utils";
 import { getAllcodeService } from "../../../services/userService";
 import "./UserRedux.scss";
 import Lightbox from "react-image-lightbox";
@@ -82,18 +82,20 @@ class UserRedux extends Component {
         role: arrRoles && arrRoles.length > 0 ? arrRoles[0].key : "",
         avatar: "",
         action: CRUD_ACTIONS.CREATE,
+        previewImgURL: "",
       });
     }
   }
 
-  handleOnchangeImage = (event) => {
+  handleOnchangeImage = async (event) => {
     let data = event.target.files;
     let file = data[0];
     if (file) {
+      let base64 = await CommonUtils.getBase64(file);
       let objectUrl = URL.createObjectURL(file);
       this.setState({
         previewImgURL: objectUrl,
-        avatar: file,
+        avatar: base64,
       });
     }
   };
@@ -121,6 +123,7 @@ class UserRedux extends Component {
         gender: this.state.gender,
         positionId: this.state.position,
         roleId: this.state.role,
+        avatar: this.state.avatar,
       });
     }
     if (action === CRUD_ACTIONS.EDIT) {
@@ -135,6 +138,7 @@ class UserRedux extends Component {
         gender: this.state.gender,
         positionId: this.state.position,
         roleId: this.state.role,
+        avatar: this.state.avatar,
       });
     }
   };
@@ -167,6 +171,10 @@ class UserRedux extends Component {
   };
 
   handleEditUserFromParent = (user) => {
+    let imageBase64 = "";
+    if (user.image) {
+      imageBase64 = new Buffer(user.image, `base64`).toString(`binary`);
+    }
     this.setState({
       email: user.email,
       password: "HARDCODE",
@@ -178,6 +186,7 @@ class UserRedux extends Component {
       position: user.positionId,
       role: user.roleId,
       avatar: "",
+      previewImgURL: imageBase64,
       action: CRUD_ACTIONS.EDIT,
       userEditId: user.id,
     });
